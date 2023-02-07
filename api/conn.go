47c67461b0d77c1ee2381fbd8b5859ce0a3c1057
Dao Pham <daopm.dean@gmail.com>
2023-02-07 22:19:01 +0700

    simple rate limiter

--- api/conn.go
+++ api/conn.go
@@ -1,17 +1,34 @@
 package api
 
-import "context"
+import (
+	"context"
+	"time"
 
-type APIConn struct{}
+	"golang.org/x/time/rate"
+)
+
+type APIConn struct {
+	rateLimiter *rate.Limiter
+}
 
 func Open() *APIConn {
-	return &APIConn{}
+	return &APIConn{
+		rateLimiter: rate.NewLimiter(rate.Every(time.Second), 2),
+	}
 }
 
 func (c *APIConn) Read(context context.Context) (string, error) {
+	if err := c.rateLimiter.Wait(context); err != nil {
+		return "", err
+	}
+
 	return "Data Result", nil
 }
 
 func (c *APIConn) Resolve(context context.Context) error {
+	if err := c.rateLimiter.Wait(context); err != nil {
+		return err
+	}
+
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/daopmdean/rate-limiter-demo
 
 go 1.19
+
+require golang.org/x/time v0.3.0
